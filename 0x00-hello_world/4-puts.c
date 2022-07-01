a6ae7f822ea26483d06c9b4709773360d5105e26
muthomipaul <muthomipaul170@gmail.com>
2022-06-30 17:29:34 -0700

    using the puts() functions

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <strings.h>
+/**
+ * main - Entry point
+ * main - prints Programming is like building a multilingual puzzle and a new line 
+ * Return: returns zero if successful
+ */
 
 int main(void)
 {
-	char output[] =  "\"Programming is like building a multilingual puzzle \n";
+	char output[] = "\"Programming is like building a multilingual puzzle\n";
+
 	puts(output);
 	return (0);
 }
