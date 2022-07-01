7d6ab64907048f957854920746d3555793da6cd5
muthomipaul <muthomipaul170@gmail.com>
2022-06-30 17:55:21 -0700

    using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print out with proper grammar, but the outcome is a piece of art,
+ * return: will be 0 if successful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
