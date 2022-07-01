3109581fe5c8ca481ddd0239236889c7bce47489
muthomipaul <muthomipaul170@gmail.com>
2022-06-30 19:18:27 -0700

    displaying sizes of different variables

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,17 @@
 #include <stdio.h>
+
 /**
- * Main -Entry point
- * The program will display the sizes of variables
- * return : will return 0 if successful
+ * main - Entry point
+ * Prints the sizes of various types depending on cpu of machine
+ * Returns 0 if successful
  */
-int main (void) 
+int main(void)
 {
-	printf(sizeof(int));
-	printf(sizeof(char));
-	printf(sizeof(float));
-	printf(sizeof(long int));
-	printf(sizeof(long long int));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(long long int));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
+
 	return (0);
 }
-
