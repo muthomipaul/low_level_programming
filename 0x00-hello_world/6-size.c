2a50153622b3ea4f14edad2d67c8853b0bac5fb2
muthomipaul <muthomipaul170@gmail.com>
2022-06-30 18:14:58 -0700

    printing variable sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * Main -Entry point
+ * The program will display the sizes of variables
+ * return : will return 0 if successful
+ */
+int main (void) 
+{
+	printf(sizeof(int));
+	printf(sizeof(char));
+	printf(sizeof(float));
+	printf(sizeof(long int));
+	printf(sizeof(long long int));
+	return (0);
+}
+
