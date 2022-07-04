c7ad0bed3172ff369b0ebdffa10012e8691955fc
muthomipaul <muthomipaul170@gmail.com>
2022-07-03 23:41:34 -0700

    printing all sinle digit intergers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Main function for printing single digit intergers
+ * Return: Always
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
