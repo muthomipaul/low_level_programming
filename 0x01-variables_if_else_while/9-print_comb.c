c0c8bcf06a17065365e0b4246ab7877e568e75e0
muthomipaul <muthomipaul170@gmail.com>
2022-07-04 00:58:43 -0700

    possible combinations of single-digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Main function for printing number of combinations of single digit intergers
+ * Return: Always
+ */
+int main(void)
+{
+	int i = 0;
+
+	do {
+		putchar((i) + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		i++;
+	} while (i < 10);
+	return (0);
+}
+
+
