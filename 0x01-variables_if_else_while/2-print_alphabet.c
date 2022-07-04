3b37391f801252471e9fc5766b1bcf0569fbdffb
muthomipaul <muthomipaul170@gmail.com>
2022-07-03 22:54:05 -0700

    printing aplhabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Main function for printing alphabetical characters
+ * Return: Always
+ */
+int main(void)
+{
+	char n;
+
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
