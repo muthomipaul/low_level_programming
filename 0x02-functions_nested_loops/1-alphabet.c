c54adbf939e6cb148be7c5ceda2c7b314d800268
muthomipaul <muthomipaul170@gmail.com>
2022-07-05 17:51:01 -0700

    printing the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * Main function for printing the alphabet
+ * Return: Always
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -3,5 +3,18 @@
 #include <unistd.h>
 
 int _putchar();
+
+// ---print_alphabet function
+void print_alphabet(void)
+{	
+	char lowerCase;
+	
+	for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+	{
+		_putchar(lowerCase);
+	}
+	_putchar('\n');	
+}
+// ------------------------
 void print_alphabet(void);
 #endif
