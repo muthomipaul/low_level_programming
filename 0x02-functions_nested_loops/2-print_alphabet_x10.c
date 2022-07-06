9e57ad43ef80626eafc78469bb745234412b6c01
muthomipaul <muthomipaul170@gmail.com>
2022-07-05 18:46:47 -0700

    pritnitng the alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+/**DbdBfgnzf
+ * print_alphabet_x10 - Entry Point
+ * Main function for printing the alphabet ten times
+ * Return: Always
+ */
+void print_alphabet_x10(void)
+{
+	char lowerCase;
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (lowerCase = 'a'; lowerCase <= 'z'; lowerCase++)
+		{
+			_putchar(lowerCase);
+		}
+	}
+	_putchar('\n');
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,4 +4,5 @@
 
 int _putchar();
 void print_alphabet();
+void print_alphabet_x10(void);
 #endif
