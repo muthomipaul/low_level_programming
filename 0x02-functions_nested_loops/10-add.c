14dc1a7ecb7216ab56e49b9e3961936fcaae42c9
muthomipaul <muthomipaul170@gmail.com>
2022-07-05 19:48:50 -0700

    adding two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - Entry Point
+ * @a: number a
+ * @b: number b
+ * Main function for prints the last digit of a number
+ * Return: Always
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
+
