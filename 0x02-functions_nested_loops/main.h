5efaed3e40cf49c1e56a7b09e7c164dee11faa70
muthomipaul <muthomipaul170@gmail.com>
2022-07-05 17:04:54 -0700

    _putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,15 +1,17 @@
- #ifndef HEADER
+#ifndef HEADER
 #define HEADER
 
 int _putchar(void)
 {
 	int i;
-
+	
 	char n[8] = "_putchar";
 	for (i = 0; i < 8; i++)
 	{
 		printf("%c", n[i]);
 	}
 	printf("\n");
+	return (0);
 }
 #endif
+
