e9ee03d92d6aa5c40c0f06153ed8a2ccbdf281cf
muthomipaul <muthomipaul170@gmail.com>
2022-07-05 19:27:46 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _abs - Entry Point
+ * @n: number n
+ * Main function for checking absolute value
+ * Return: Always
+ */
+int _abs(int n)
+{
+	return (abs(n));
+}
+
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,4 +6,5 @@ int _putchar();
 void print_alphabet();
 void print_alphabet_x10(void);
 int _islower(int c);
+int _abs(int);
 #endif
