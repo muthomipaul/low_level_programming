404baaab0f43ec5660cbd7ce0b86be24094bd310
muthomipaul <muthomipaul170@gmail.com>
2022-07-07 14:24:36 -0700

    largest number

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,24 +1,34 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
-#include "main.h"
 
+void positive_or_negative(int i);
 /**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
+ * largest_number - returns the largest of 3 numbers
+ *  @a: first integer
+ *  @b: second integer
+ *  @c: third integer
+ *  Return: largest number
+ */
+int largest_number(int a, int b, int c);
+/**
+ * convert_day - converts day of month to day of year
+ * for leap year
+ * @month: month in number format
+ * @day: day of month
+ * Return: day of year
+ */
+int convert_day(int month, int day);
+/**
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
+ * @day: day of month
+ * @year: year
+ * Return: void
+ */
+void print_remaining_days(int month, int day, int year);
 
-int main(void)
-{
-        int a, b, c;
-        int largest;
-
-        a = 972;
-        b = -98;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
+#endif /* MAIN_H */
 
