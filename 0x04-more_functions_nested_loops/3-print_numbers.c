5777b353ae2ba1292e466845f6f569f3bc12037b
muthomipaul <muthomipaul170@gmail.com>
2022-07-07 17:23:20 -0700

    printing numbers 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * mul - Entry Point
+ * @a: number a
+ * @b: number b
+ * function for multiplying two numbers
+ * Return: Always
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n')
+}
+
