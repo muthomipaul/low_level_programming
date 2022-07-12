7338a19d8715860854c007f1033b783aefc3053f
muthomipaul <muthomipaul170@gmail.com>
2022-07-11 17:47:33 -0700

    updaing value of parameter

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry Point
+ * @n: integer n
+ * reset_to_98 function for updating a parameter to 98
+ * Return: Always
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,7 @@
+#ifndef HEADER
+#define HEADER
+
+void reset_to_98(int *n);
+
+#endif
+
