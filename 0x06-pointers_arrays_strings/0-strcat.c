79952c13c557dfb05791e49291df6d8c128b863f
muthomipaul <muthomipaul170@gmail.com>
2022-07-14 15:35:48 -0700

    concatenating two strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strcat - Entry Point
+ * @dest: character dest
+ * @src: character src
+ * Main function for concatenating two strings
+ * Return: Always
+ */
+char *_strcat(char *dest, char *src)
+{
+	strcat(dest, src);
+
+	return (dest)
+}
+
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef HEADER
+#define HEADER
+
+char *_strcat(char *dest, char *src);
+
+#endif
