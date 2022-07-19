3151b9c6ef664600d7ccf30a9fb280f3eb9b0641
muthomipaul <muthomipaul170@gmail.com>
2022-07-19 16:34:19 -0700

    filling the memory with a constant byte

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,46 +4,25 @@
 #include "main.h"
 
 /**
- * simple_print_buffer - Entry Point
- * @buffer: character buffer
- * @size: int size
+ * _memset - Entry Point
+ * @s: character s
+ * @b: character b
+ * @n: int n
  * Main function for concatenating two strings
  * Return: Always
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-	unsigned int i;
 
-	i = 0;
-	while (i < size)
+char *_memset(char *s, char b, unsigned int n)
+{
+
+	int size = n, i;
+
+	if (size > 0)
 	{
-		if (i % 10)
-		{
-			printf(" ");
-		}
-		if (!(i % 10) && i)
-		{
-			printf("\n");
-		}
-		printf("0x%02x", buffer[i]);
-		i++;
+		for (i = 0; i < size; i++)
+			s[i] = b;
 	}
-	printf("\n");
-}
 
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	char buffer[98] = {0x00};
-
-	simple_print_buffer(buffer, 98);
-	_memset(buffer, 0x01, 95);
-	printf("-------------------------------------------------\n");
-	simple_print_buffer(buffer, 98);
-	return (0);
+	return (s);
 }
 
