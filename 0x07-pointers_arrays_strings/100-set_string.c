1f2d563bdd385497a25dc6250dcaa6cfd32843da
muthomipaul <muthomipaul170@gmail.com>
2022-07-19 17:42:47 -0700

    value of a pointer to a char

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * set_string - Entry Point
+ * @s: character s
+ * @to: character to
+ * Main function for concatenating two strings
+ * Return: Always
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
+
