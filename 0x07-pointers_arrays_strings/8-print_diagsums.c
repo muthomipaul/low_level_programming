7a31c7e0a11d3e7d4e7f219cb45e0b1227ed21d1
muthomipaul <muthomipaul170@gmail.com>
2022-07-19 17:12:08 -0700

    sum of diagonals

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_diagsums - Entry Point
+ * @a: int a
+ * @size: int size
+ * Main function for concatenating two strings
+ * Return: Always
+ */
+void print_diagsums(int *a, int size)
+{
+
+	int diagonal_sum_1 = 0;
+	int diagonal_sum_2 = 0;
+	int row, i;
+
+	for (row = 0; row < size; row++)
+	{
+		i = (row * size) + row;
+		diagonal_sum_1 += a[i];
+	}
+
+	for (row = 1; row <= size; row++)
+	{
+		i = (row * size) - row;
+		diagonal_sum_2 += a[i];
+	}
+
+	printf("%d, %d\n", diagonal_sum_1, diagonal_sum_2);
+
+}
+
