606917cc6553c4a0c9c6ef0aaa3bc1ef2458358f
muthomipaul <muthomipaul170@gmail.com>
2022-07-20 19:07:40 -0700

    checking for prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -6,20 +6,33 @@
 /**
  * is_prime_number - Entry Point
  * @n: int n
+ * @i: int i
  * Return: Always
  */
+int prime_checker(int n, int i);
+
 int is_prime_number(int n)
 {
-	int divider;
-
-	if (n == divider)
-	{
-		return (1);
-	}
-	if (n % divider == 0)
-	{
+	if (n <= 1)
 		return (0);
-	}
-	return (is_prime_number(n, divider + 1));
+	else if (prime_checker(n, n / 2) > 0)
+		return (1);
+	return (0);
+}
+
+/**
+ * prime_checker - checks for prime number
+ * @n: int n
+ * @i: int i
+ * Return: Always
+ */
+int prime_checker(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	else
+		return (prime_checker(n, i - 1));
 }
 
