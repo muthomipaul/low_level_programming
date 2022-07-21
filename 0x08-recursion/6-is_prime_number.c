2c98faf6bda68ed490c02f160dd7d4e7778d05a7
muthomipaul <muthomipaul170@gmail.com>
2022-07-20 18:37:16 -0700

    checking if its a prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,35 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * is_prime_number - Entry Point
+ * @n: int n
+ * Return: Always
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	else if (prime_checker(n, n / 2) > 0)
+		return (1);
+	return (0);
+}
+
+/**
+ * prime_checker - checks for prime number
+ * @n: int n
+ * @i: int i
+ * Return: Always
+ */
+int prime_checker(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0)
+		return (0);
+	else
+		return (prime_checker(n, i - 1));
+}
+
