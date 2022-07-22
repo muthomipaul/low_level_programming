b862016f472c98c05de5b447165bb5c7410199b1
muthomipaul <muthomipaul170@gmail.com>
2022-07-22 16:06:01 -0700

    printing number of arguments

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry Point
+ * @argc: int argc argv
+ * @argv: char
+ * main function
+ * Return: Always
+ */
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+	}
+	printf("%d\n", i - 1);
+	return (0);
+}
+
