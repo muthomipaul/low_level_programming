9cd6ecc43b2a9aed6f60b2bf19d8029d11e105d3
muthomipaul <muthomipaul170@gmail.com>
2022-07-26 15:10:33 -0700

    pointer to a newly allocated space in memory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strdup - Entry Point
+ * @str: char str
+ * 
+ * Return: Always
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *dup;
+
+	dup = (char *) malloc((i + 1) * sizeof(char));
+	if (str == NULL)
+	{
+		return (str);
+	}
+	for (i = 0; str[i]; ++i)
+		;
+	if (dup == NULL)
+	{
+		return (dup);
+	}
+	for (j = 0; j <= i; ++j)
+	{
+		dup[j] = str[j];
+	}
+	return (dup);
+}
+
