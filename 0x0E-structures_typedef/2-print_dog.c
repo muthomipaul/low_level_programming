a317f1e1eae2881821f2a2f24154facfe863ddfc
muthomipaul <muthomipaul170@gmail.com>
2022-08-01 18:45:18 -0700

    printing struct dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "dog.h"
+/**
+ * main - Entry Point
+ * Return: Always
+ */
+int main(void)
+{
+	struct dog my_dog;
+
+	my_dog.name = "Django";
+	my_dog.age = 3.5;
+	my_dog.owner = "Jay";
+	print_dog(&my_dog);
+	return (0);
+}
+
