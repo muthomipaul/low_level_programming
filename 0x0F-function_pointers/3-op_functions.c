47b03c0412a931dfd796257cdee88d13833753c8
muthomipaul <muthomipaul170@gmail.com>
2022-08-04 13:11:54 -0700

    push

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,59 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * op_add - function add
+ * @b: int b
+ * @a: int a
+ * Return: Always
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - function subtract
+ * @b: int b
+ * @a: int a
+ * Return: Always
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - function multiply
+ * @b: int b
+ * @a: int a
+ * Return: Always
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - function divide
+ * @b: int b
+ * @a: int a
+ * Return: Always
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - function modulus
+ * @b: int b
+ * @a: int a
+ * Return: Always
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
+
