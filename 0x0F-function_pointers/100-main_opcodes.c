71139ce761280f7042d06582ece75e42e2f6a7d7
muthomipaul <muthomipaul170@gmail.com>
2022-08-04 13:14:22 -0700

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,39 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry Point
+ * @argc: int argc
+ * @argv: char argv
+ * Return: Always
+ */
+int main(int argc, char *argv[])
+{
+	int index, nbytes;
+	char *ptr = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < nbytes; index++)
+	{
+		printf("%02x", ptr[index] & 0xFF);
+		if (index != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
+	return (0);
+}
+
