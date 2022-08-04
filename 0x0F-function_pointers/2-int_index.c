a94b323752ed3ce04bb9a0c3c43303c7c1bc22e7
muthomipaul <muthomipaul170@gmail.com>
2022-08-04 11:51:44 -0700

    searching for an integer

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,23 +11,17 @@
  */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int index;
-	bool y;
+	int i;
 
-	if (array != NULL && size > 0 && cmp != NULL)
+	if (size < 1 || array == NULL || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
 	{
-		if (size <= 0)
-			return (-1);
-
-		for (index = 0; index < size; index++)
-		{
-			y = cmp(array[index]);
-			if (y == TRUE)
-				return (index);
-		}
+		if (cmp(array[i]))
+			return (i);
 	}
 
 	return (-1);
-
 }
 
