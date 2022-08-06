c9679db7fdb3af7fe5cfdcec31702f6cc46f34a8
muthomipaul <muthomipaul170@gmail.com>
2022-08-05 18:40:28 -0700

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,99 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+ * print_char - Entry Point
+ * @arg: argument
+ * Return: Always
+ */
+void print_char(va_list arg)
+{
+	char c = va_arg(arg, int);
+
+	printf("%c", c);
+}
+
+/**
+ * print_int - Entry Point
+ * @arg: argument
+ * Return: Always
+ */
+
+void print_int(va_list arg)
+{
+	int n = va_arg(arg, int);
+
+	printf("%d", n);
+}
+
+/**
+ * print_float - Entry Point
+ * @arg: argument
+ * Return: Always
+ */
+
+void print_float(va_list arg)
+{
+	float n = va_arg(arg, double);
+
+	printf("%f", n);
+}
+
+/**
+ * print_string - Entry Point
+ * @arg: argument
+ * Return: Always
+ */
+
+void print_string(va_list arg)
+{
+	char *str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", str);
+}
+
+/**
+ * print_all - Entry Point
+ * @format: char format
+ * Return: Always
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list ap;
+	int i = 0, j = 0;
+	char *separator = "";
+	func_printer funcs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(ap, format);
+
+	while (format && format[i])
+	{
+		j = 0;
+		while (j < 4 && (format[i] != *(funcs[j].symbol)))
+			j++;
+		if (j < 4)
+		{
+			printf("%s", separator);
+			funcs[j].print_func(ap);
+			separator = ", ";
+		}
+		i++;
+	}
+	printf("\n");
+
+	va_end(ap);
+}
+
