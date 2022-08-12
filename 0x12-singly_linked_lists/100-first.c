dd5ad5538e0c8f7b0e4ab84cbebd5cf44d19a2c5
muthomipaul <muthomipaul170@gmail.com>
2022-08-11 18:16:21 -0700

    premain

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * premain - runs before main
+ *
+ */
+void __attribute__((constructor)) premain()
+{
+	printf(
+	    "You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
