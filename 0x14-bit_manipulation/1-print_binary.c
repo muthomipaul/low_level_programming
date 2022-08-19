7f93817e4529372fec6184051f73c1b93a244ce3
muthomipaul <muthomipaul170@gmail.com>
2022-08-18 17:35:08 -0700

    printing binary

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,17 @@
+/**
+ * print_binary - Entry Point
+ * @n: int n
+ *
+ * Return: Always
+ */
+void print_binary(unsigned long int n)
+{
+	int printbin = ((n & 1) + '0');
+
+	if (n > 1)
+	{
+		print_binary(n >> 1);
+	}
+	_putchar(printbin);
+}
+
