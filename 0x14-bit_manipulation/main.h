4ba07ca479071e500f949a608cbcd8cce24db14a
muthomipaul <muthomipaul170@gmail.com>
2022-08-18 17:39:50 -0700

    printing binary

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -4,11 +4,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
+int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int get_bit(unsigned long int n, unsigned int index);
